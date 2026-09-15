889c22298f61d86f720cf01ca03a33966d831db3
PREMKUMAR <167052615+premkumar29820@users.noreply.github.com>
2026-09-15 18:25:12 +0530

    Update Blogs.jsx

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -4,8 +4,7 @@ import Footer from "./common/Footer";
 import auth from "../config/firebase";
 
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE_URL = "http://localhost:10000";
 
 const API = `${API_BASE_URL.replace(/\/$/, "")}/api`;
 
