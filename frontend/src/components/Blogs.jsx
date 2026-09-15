f3438e40ec996f9a49a163781d542d3b3a4faa4d
PREMKUMAR <167052615+premkumar29820@users.noreply.github.com>
2026-09-15 18:40:53 +0530

    Update Blogs.jsx

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -4,7 +4,7 @@ import Footer from "./common/Footer";
 import auth from "../config/firebase";
 
 
-const API_BASE_URL = "http://localhost:10000";
+const API_BASE_URL ="http://localhost:10000";
 
 const API = `${API_BASE_URL.replace(/\/$/, "")}/api`;
 
@@ -345,6 +345,4 @@ function Blogs() {
   );
 }
 
-
 export default Blogs;
-
